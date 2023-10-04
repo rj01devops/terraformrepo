1d98a151748284de99d94b7e3f5e50a0cf8e5751
rj01devops <140480590+rj01devops@users.noreply.github.com>
2023-10-04 09:53:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@ terraform {
     }
   }
 }
-variable "AWS_ACCESS_KEY" {}
-variable "AWS_SECRET_KEY" {}
+variable "ACCESSKEY" {}
+variable "SECRETKEY" {}
 provider "aws" {
-access_key = var.AWS_ACCESS_KEY
-secret_key = var.AWS_SECRET_KEY
+access_key = var.ACCESSKEY
+secret_key = var.SECRETKEY
   region     = "ap-south-1"
 }
 resource "aws_instance" "webec2" {
